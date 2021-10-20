542d515d46ce8bc8397d533cb348c0d4829ffc26
awkto <awkto@outlook.com>
2021-10-20 13:13:09 -0400

    finished part 1

--- client/api/index.js
+++ client/api/index.js
@@ -1,7 +1,1 @@
-import axios from 'axios';
-
-const url = 'http://localhost:5000/posts'
-
-//create a function
-export const fetchPosts = () => axios.get(url);
 
--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -19,3 +19,13 @@ export const getPosts = () => async (dispatch) => {
     // dispatch(action);    
 }
 // use redux thunk allows us to specify an additional function
+
+export const createPost = (post) => async (dispatch) => {
+    try {
+        const { data } = await api.createPost(post);
+
+        dispatch({ type: 'CREATE', payload: data});
+    } catch (error) {
+        console.log(error);
+    }
+}
